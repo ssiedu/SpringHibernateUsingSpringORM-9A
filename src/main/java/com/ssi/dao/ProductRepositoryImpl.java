67d67370b2c59769a878f2dabbd922271e1d0e53
manoj <manojs121@rediffmail.com>
2021-07-05 10:27:00 +0530

    050721

---
+++ src/main/java/com/ssi/dao/ProductRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.ssi.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import com.ssi.model.Product;
+
+@Repository
+public class ProductRepositoryImpl implements ProductRepository {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	public Product searchProduct(int code) {
+		Session session=sessionFactory.openSession();
+		Product product=session.get(Product.class, code);
+		session.close();
+		return product;
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Spring-ORM-Example</h2>
+<hr>
+	<a href="searchinput">Search-Product</a>
+<hr>
+</body>
+</html>
